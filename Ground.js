bfe9a1bbdd21fdbb97192dd76dc2021db3978d1a
AnushkaPatil16 <patilanushka9909@gmail.com>
2021-02-22 19:52:44 +0530

    random

--- Ground.js
+++ Ground.js
@@ -8,10 +8,12 @@ class Ground {
       this.height = height;
       World.add(world, this.body);
     }
+
+
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
       fill("brown");
       rect(pos.x, pos.y, this.width, this.height);
     }
-  };
+  }
